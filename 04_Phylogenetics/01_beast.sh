d4caed629df940db90a73af9d529a9d88a6591c3
Lauren Hennelly <lmhennelly@ucdavis.edu>
2023-06-24 15:27:02 +0200

    Update 01_beast.sh

--- 04_Phylogenetics/01_beast.sh
+++ 04_Phylogenetics/01_beast.sh
@@ -3,9 +3,9 @@
 #SBATCH --time 4:00:00
 #SBATCH --mem=1GB
 #SBATCH -p high
-#SBATCH -o beast_June24.out
-#SBATCH -o beast_June24.err
+#SBATCH -o beast_June24_adjsd.out
+#SBATCH -o beast_June24_adjsd.err
 
 conda init beastie1
 
-beast BNS_PakistanwolvesNov1_finalJun24.xml
+beast BNS_PakistanwolvesNov1_finalJun24_adjsd.xml
