82ffbc85856e603e0771c3d98af1950eae678833
Lauren Hennelly <lmhennelly@ucdavis.edu>
2023-07-01 18:48:48 +0200

    Update 02_treeannotator.sh

--- 04_Phylogenetics/02_treeannotator.sh
+++ 04_Phylogenetics/02_treeannotator.sh
@@ -3,10 +3,10 @@
 #SBATCH --time 1-24:00:00
 #SBATCH --mem=15GB
 #SBATCH -p high
-#SBATCH -o treeannotator_Jun24.out
-#SBATCH -o treeannotator_Jun24.err
+#SBATCH -o treeannotator_July1.out
+#SBATCH -o treeannotator_July1.err
 #SBATCH --exclude=c10-96,c10-69,c11-76,c11-93,c11-86
 
 conda init beastie1
 
-/home/hennelly/miniconda3/envs/beastie1/bin/treeannotator -burninTrees 5000 BNS_PakistanwolvesNov1_finalJun24_adjsd.trees.txt BNS_PakistanwolvesNov1__adjsd_Jun24beastFINALTREE.txt
+/home/hennelly/miniconda3/envs/beastie1/bin/treeannotator -burninTrees 5000 BNS_PakistanwolvesNov1_finalJuly1_default.trees.txt BNS_PakistanwolvesNov1_finalJuly1_defaultFINALTREE.txt
